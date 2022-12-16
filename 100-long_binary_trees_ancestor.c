88bb6b0a343824ebe90aab4c8c8307115c7efc9e
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2022-12-16 01:21:43 +0100

    100-long_binary_trees_ancestor

--- 100-long_binary_trees_ancestor.c
+++ 100-long_binary_trees_ancestor.c
@@ -6,7 +6,6 @@ size_t depth(const binary_tree_t *tree);
  * binary_trees_ancestor - Finds the lowest common ancestor of two nodes.
  * @first: Pointer to the first node.
  * @second: Pointer to the second node.
- *
  * Return: If no common ancestors return NULL, else return common ancestor.
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
