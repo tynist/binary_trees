ea53ab5f6089deb97c3832e110d60a001fa1bf9a
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2022-12-16 01:48:46 +0100

    131-heap_insert

--- 131-heap_insert.c
+++ 131-heap_insert.c
@@ -4,7 +4,6 @@
  * heap_insert - inserts a value in Max Binary Heap
  * @root: a double pointer to the root node of the Heap to insert the value
  * @value: the value to store in the node to be inserted
- *
  * Return: a pointer to the created node
  *         NULL on failure
  */
