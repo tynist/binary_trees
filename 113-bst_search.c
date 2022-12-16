deddd3ba7debd805546d52f14f24cdd23ad01423
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2022-12-16 01:41:22 +0100

    113-bst_search.c

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * bst_search - Searches for a value in a binary search tree.
+ * bst_search: Searches for a value in a binary search tree.
  * @tree: A pointer to the root node of the BST to search.
  * @value: The value to search for in the BST.
  * Rurn: If the tree is NULL or the value is not found, NULL.
