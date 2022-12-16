c684e8a645c38f4501d7c7571f1ebd180496b9b3
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2022-12-16 01:26:37 +0100

    11-binary_tree_size

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -3,7 +3,6 @@
 /**
  * binary_tree_size - Measures the size of a binary tree.
  * @tree: A pointer to the root node of the tree to measure the size of.
- *
  * Return: The size of the tree.
  */
 size_t binary_tree_size(const binary_tree_t *tree)
