acf0ff82e974e8b934cc42af8cf27b5cf2df83be
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2022-12-16 01:50:32 +0100

    132-array_to_heap

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -4,7 +4,6 @@
 /**
  * tree_height - measures the height of a binary tree
  * @tree: pointer to the root node of the tree to measure the height
- *
  * Return: Height or 0 if tree is NULL
  */
 size_t tree_height(const heap_t *tree)
